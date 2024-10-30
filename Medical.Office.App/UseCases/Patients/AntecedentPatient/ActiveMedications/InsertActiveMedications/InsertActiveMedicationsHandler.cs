c7a43a2b43eba165e9853491a0ff9007cce88e55
Rogelio Arriaga Gonzalez <rogelioarriaga@live.com.mx>
2024-10-29 21:58:03 -0500

    Update

--- Medical.Office.App/UseCases/Patients/AntecedentPatient/ActiveMedications/InsertActiveMedications/InsertActiveMedicationsHandler.cs
+++ Medical.Office.App/UseCases/Patients/AntecedentPatient/ActiveMedications/InsertActiveMedications/InsertActiveMedicationsHandler.cs
@@ -1,18 +1,36 @@
 ﻿using Common.Common.CleanArch;
+using Medical.Office.App.Dtos.Patients.AntecedentPatient.ActiveMedications;
 using Medical.Office.App.UseCases.Patients.AntecedentPatient.ActiveMedications.InsertActiveMedications.Responses;
+using Medical.Office.Domain.Repository;
 
 namespace Medical.Office.App.UseCases.Patients.AntecedentPatient.ActiveMedications.InsertActiveMedications
 {
     internal sealed class InsertActiveMedicationsHandler : IInteractor<InsertActiveMedicationsRequest, InsertActiveMedicationsResponse>
     {
-        public InsertActiveMedicationsHandler()
+        private readonly IAntecedentPatient _patient;
+
+        public InsertActiveMedicationsHandler(IAntecedentPatient patient)
         {
-            
+            _patient=patient;
         }
 
         public async Task<InsertActiveMedicationsResponse> Handle(InsertActiveMedicationsRequest request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+           await _patient.InsertActiveMedicationsAsync(request.IDPatient,request.ActiveMedicationsData);
+           var GetLastActiveMedications = await _patient.GetActiveMedicationsByPatientIdAsync(request.IDPatient);
+            if (GetLastActiveMedications == null) 
+            { 
+                return new FailureInsertActiveMedicationsResponse("Falla al agregar informacion del paciente");
+            }
+            if (!GetLastActiveMedications.IDPatient.Equals(request.IDPatient))
+            {
+                return new FailureInsertActiveMedicationsResponse("Error en obtener datos");
+            }
+            return new SuccessInsertActiveMedicationsResponse(new ActiveMedicationsDto(
+                GetLastActiveMedications.Id,
+                GetLastActiveMedications.IDPatient,
+                GetLastActiveMedications.AactiveMedicationsData,
+                GetLastActiveMedications.DateTimeSnap));
         }
     }
 }
