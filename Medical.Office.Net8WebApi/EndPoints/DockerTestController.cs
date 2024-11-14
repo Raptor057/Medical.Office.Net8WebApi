91ff3d176888542bacd86e8925d815c0163217e3
Rogelio Arriaga <r.arriaga@generaltransmissions.com>
2024-11-13 18:59:53 -0600

    Update

---
+++ Medical.Office.Net8WebApi/EndPoints/DockerTestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medical.Office.Net8WebApi.EndPoints
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DockerTestController : ControllerBase
+    {
+        private readonly Random _random;
+
+        public DockerTestController()
+        {
+            _random = new Random();
+        }
+
+        [HttpGet]
+        [Route("/api/getrandomnumber")]
+        public IActionResult GetRandomNumber()
+        {
+            int randomNumber = _random.Next();
+            return Ok(randomNumber);
+        }
+    }
+}
