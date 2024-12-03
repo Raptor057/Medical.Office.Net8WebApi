68de584db3d9417f0c85e65e36f5495287e348fb
Rogelio Arriaga Gonzalez <rogelioarriaga@live.com.mx>
2024-12-03 00:18:00 -0600

    Update

---
+++ Medical.Office.Infra/Repositories/PosRepository.cs
@@ -0,0 +1,357 @@
+using Medical.Office.Domain.Entities.POS;
+using Medical.Office.Domain.Repository;
+
+namespace Medical.Office.Infra.Repositories;
+
+public class PosRepository : IPosRepository
+{
+    public Task<IEnumerable<CashMovements>> GetCashMovementsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CashMovements> GetCashMovementByIdAsync(CashMovements cashMovements)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertCashMovementAsync(CashMovements cashMovements)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateCashMovementAsync(CashMovements cashMovements)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteCashMovementAsync(CashMovements cashMovements)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<CashRegisters>> GetCashRegistersAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CashRegisters> GetCashRegisterByIdAsync(CashRegisters cashRegisters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertCashRegisterAsync(CashRegisters cashRegisters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateCashRegisterAsync(CashRegisters cashRegisters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteCashRegisterAsync(CashRegisters cashRegisters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<InventoryMovements>> GetInventoryMovementsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<InventoryMovements> GetInventoryMovementByIdAsync(InventoryMovements inventoryMovements)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertInventoryMovementAsync(InventoryMovements inventoryMovements)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateInventoryMovementAsync(InventoryMovements inventoryMovements)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteInventoryMovementAsync(InventoryMovements inventoryMovements)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<PaymentTypes>> GetPaymentTypesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PaymentTypes> GetPaymentTypeByIdAsync(PaymentTypes paymentTypes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertPaymentTypeAsync(PaymentTypes paymentTypes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdatePaymentTypeAsync(PaymentTypes paymentTypes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeletePaymentTypeAsync(PaymentTypes paymentTypes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ProductCategories>> GetProductCategoriesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProductCategories> GetProductCategoryByIdAsync(ProductCategories productCategories)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertProductCategoryAsync(ProductCategories productCategories)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateProductCategoryAsync(ProductCategories productCategories)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteProductCategoryAsync(ProductCategories productCategories)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Products>> GetProductsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Products> GetProductByIdAsync(Products products)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertProductAsync(Products products)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateProductAsync(Products products)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteProductAsync(Products products)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Promotions>> GetPromotionsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Promotions> GetPromotionByIdAsync(Promotions promotions)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertPromotionAsync(Promotions promotions)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdatePromotionAsync(Promotions promotions)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeletePromotionAsync(Promotions promotions)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ReturnDetails>> GetReturnDetailsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ReturnDetails> GetReturnDetailByIdAsync(ReturnDetails returnDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertReturnDetailAsync(ReturnDetails returnDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateReturnDetailAsync(ReturnDetails returnDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteReturnDetailAsync(ReturnDetails returnDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ReturnsProducts>> GetReturnsProductsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ReturnsProducts> GetReturnProductByIdAsync(ReturnsProducts returnsProducts)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertReturnProductAsync(ReturnsProducts returnsProducts)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateReturnProductAsync(ReturnsProducts returnsProducts)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteReturnProductAsync(ReturnsProducts returnsProducts)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ReturnStatuses>> GetReturnStatusesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ReturnStatuses> GetReturnStatusByIdAsync(ReturnStatuses returnStatuses)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertReturnStatusAsync(ReturnStatuses returnStatuses)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateReturnStatusAsync(ReturnStatuses returnStatuses)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteReturnStatusAsync(ReturnStatuses returnStatuses)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<SaleDetails>> GetSaleDetailsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SaleDetails> GetSaleDetailByIdAsync(SaleDetails saleDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertSaleDetailAsync(SaleDetails saleDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateSaleDetailAsync(SaleDetails saleDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteSaleDetailAsync(SaleDetails saleDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Sales>> GetSalesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Sales> GetSaleByIdAsync(Sales sales)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertSaleAsync(Sales sales)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateSaleAsync(Sales sales)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteSaleAsync(Sales sales)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<SalesPromotions>> GetSalesPromotionsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SalesPromotions> GetSalesPromotionByIdAsync(SalesPromotions salesPromotions)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertSalesPromotionAsync(SalesPromotions salesPromotions)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateSalesPromotionAsync(SalesPromotions salesPromotions)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteSalesPromotionAsync(SalesPromotions salesPromotions)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<SaleStatuses>> GetSaleStatusesAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SaleStatuses> GetSaleStatusByIdAsync(SaleStatuses saleStatuses)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task InsertSaleStatusAsync(SaleStatuses saleStatuses)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateSaleStatusAsync(SaleStatuses saleStatuses)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteSaleStatusAsync(SaleStatuses saleStatuses)
+    {
+        throw new NotImplementedException();
+    }
+}
