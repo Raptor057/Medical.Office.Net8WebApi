197301ba21b81bf1f98a5d4d150b74f26908bca5
Rogelio Arriaga <r.arriaga@generaltransmissions.com>
2024-12-17 16:36:10 -0600

    update IHostedService to async

--- Medical.Office.App/Services/BackgroundService/MedicalAppointmentCalendarHostedService.cs
+++ Medical.Office.App/Services/BackgroundService/MedicalAppointmentCalendarHostedService.cs
@@ -17,13 +17,13 @@ namespace Medical.Office.App.Services.BackgroundService
             _patients = patients;
         }
 
-        public Task StartAsync(CancellationToken cancellationToken)
+        public async Task StartAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Medical Appointment Calendar Background Worker started.");
 
             // Configura el Timer para ejecutar la tarea periódicamente.
             _timer = new Timer(DoWork, null, TimeSpan.Zero, TimeSpan.FromMinutes(5));
-            return Task.CompletedTask;
+            await Task.CompletedTask;
         }
 
         private async void DoWork(object? state)
@@ -32,11 +32,11 @@ namespace Medical.Office.App.Services.BackgroundService
             _logger.LogInformation($"Appointment Status Update: {DateTimeOffset.Now}");
         }
 
-        public Task StopAsync(CancellationToken cancellationToken)
+        public async Task StopAsync(CancellationToken cancellationToken)
         {
             _logger.LogWarning("Medical Appointment Calendar Background Worker is stopping.");
             _timer?.Change(Timeout.Infinite, 0); // Detiene el Timer.
-            return Task.CompletedTask;
+            await Task.CompletedTask;
         }
     }
 }
